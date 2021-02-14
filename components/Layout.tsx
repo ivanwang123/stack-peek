cca186ec12b571090eb546cb3d95886183d12531
ivanwang123 <ivanwang02@gmail.com>
2021-02-14 10:24:43 -0500

    Add favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,6 +12,7 @@ const Layout = ({ children, title = "Stack Peek" }: PropTypes) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" href="/favicon.ico" />
     </Head>
     {children}
   </div>
